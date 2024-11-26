4037b8a4781112c0bf0b191305bd365492919cc3
Mohammed Sholaja <mohammed.sholaja@gmail.com>
2024-11-26 11:08:08 -0600

    Divider Scroll indicator count

---
+++ BBQuotes17/CharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterView.swift
+//  BBQuotes17
+//
+//  Created by mohammed sholaja on 11/26/24.
+//
+
+import SwiftUI
+
+struct CharacterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterView()
+}
